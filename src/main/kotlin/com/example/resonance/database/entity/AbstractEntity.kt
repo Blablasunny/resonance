148e7698063fda496c744d5986da2f93fb87ce38
Blablasunny <blasunnybla@mail.ru>
2025-12-21 15:41:14 +0300

    v 0.1

---
+++ src/main/kotlin/com/example/resonance/database/entity/AbstractEntity.kt
@@ -0,0 +1,27 @@
+package com.example.resonance.database.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.MappedSuperclass
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.LocalDateTime
+import java.util.UUID
+
+@MappedSuperclass
+abstract class AbstractEntity {
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: UUID? = null
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    val createdAt: LocalDateTime = LocalDateTime.now()
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    val updatedAt: LocalDateTime = LocalDateTime.now()
+}
