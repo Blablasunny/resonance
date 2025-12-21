89c4056493fd5c337113a75da951d1761339e7d5
Blablasunny <blasunnybla@mail.ru>
2025-12-21 14:12:26 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/resonance/ResonanceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.resonance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ResonanceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ResonanceApplication>(*args)
+}
